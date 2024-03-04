144d3cb2a80d9f30d0a759f532dcc9c19fd1848e
Rohed-Appnap <rohed@appnap.io>
2024-03-04 18:55:01 +0600

    Ui Updated

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,9 +2,11 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
-      <UserButton afterSignOutUrl="/" />
+    <div className="flex justify-between px-[7.25rem] py-[4rem] bg-gray-300">
+      <p className="font-bold text-[1rem]">Home</p>
+      <div className="bg-white rounded-full border w-[5rem] h-[5rem] flex items-center justify-center">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
